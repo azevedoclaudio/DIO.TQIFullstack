cff1ff1bd8e1542f3e3d0bb805743c302f32b098
Claudio Azevedo <azevedo.claudiosavio@gmail.com>
2022-07-09 14:03:02 -0300

    Pastas

--- Java/teste.js
+++ Java/teste.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+function returnevenvalues(array) {
+    let evennums = [];
+    for(let i = 0; i < array.lenght; i++) {
+        if(array[i] %2 === 0) {
+            evennums.push(array[i]);
+        }
+    }
+    console.log(evennums);
+}
+
+let array = [1, 2, 4, 5, 7, 8];
+
+returnevenvalues(array);
